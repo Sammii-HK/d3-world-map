60ca1b1bac4da40e9e7a3686e29e732682abbd84
Sammii-HK <sammii.h@icloud.com>
2020-01-31 14:09:23 +0000

    placenames on map converted to shapes currently

--- public/main.js
+++ public/main.js
@@ -1,36 +1,52 @@
 let geojson
+const w = 1500
+const h = 500
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log("JS loaded")
 
 
   d3.json('/assets/world.json', function(err, json) {
-    // createMap(json);
+    // createMap(json)
     geojson = json
-    console.log(geojson);
-    update(geojson);
+    console.log(geojson)
+    update(geojson)
+  })
+
+  d3.json('/assets/place-names.json', function(err, json) {
+    // createMap(json)
+    namesjson = json
+    console.log(namesjson)
+    update(namesjson)
   })
 
   var context = d3.select('body')
                   .append('canvas')
+                  .attr("width", w)
+                  .attr("height", h)
                   .node()
-                  .getContext('2d');
+                  .getContext('2d')
 
   var projection = d3.geoEquirectangular()
-    .scale(200)
-    .translate([200, 150]);
+    // .scale(100)
+    // .center([0, 0])
+    .translate([0, 0])
+    // .fitSize([900, 500], geojson)
+
 
   var geoGenerator = d3.geoPath()
     .projection(projection)
-    .context(context);
+    .context(context)
 
   function update(geojson) {
-    context.lineWidth = 0.5;
-    context.strokeStyle = '#aaa';
+    projection.fitExtent([[0, 0], [w, h]], geojson)
 
-    context.beginPath();
+    context.lineWidth = 0.5
+    context.strokeStyle = '#333'
+
+    context.beginPath()
     geoGenerator({type: 'FeatureCollection', features: geojson.features})
-    context.stroke();
+    context.stroke()
   }
 
 })
